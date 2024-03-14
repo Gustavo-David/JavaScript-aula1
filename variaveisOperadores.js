dc62efcb5dfc2ce593d25c5d3bd02abb771cbaef
GUSTAVO DAVID PINTO <gustavo.david@a.ucb.br>
2024-03-14 10:08:06 -0300

    first commit

---
+++ variaveisOperadores.js
@@ -0,0 +1,18 @@
+let num_1 = 10;
+let num_2 = 2;
+var result;
+
+result = num_1 + num_2;
+console.log("resultado soma: " + result);
+
+result = num_1 - num_2;
+console.log("resultado subtração: " + result);
+
+
+result = num_1 / num_2;
+console.log("resultado divisao: " + result);
+
+
+result = num_1 * num_2;
+console.log("resultado multiplicação: " + result);
+
